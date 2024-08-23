1b681ca61da1e79b7aa37bce768f948515cf957d
ANDRÉ LUIZ GOMES GUIMARÃES <andre_lg_guimaraes@estudante.sesisenai.org.br>
2024-08-22 21:45:47 -0300

    Sua mensagem

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+export function footer() {
+
+return (
+    <footer>
+          <div class="linha" ></div>            
+          <a href="instagram.com" class="coloridoazul" id="insta">Instagram</a>
+          &nbsp;&nbsp;&nbsp;&nbsp;          
+          <a href="whatsapp.com" class="coloridoazul">Fale Conosco</a>    
+      </footer>
+    )
+}
